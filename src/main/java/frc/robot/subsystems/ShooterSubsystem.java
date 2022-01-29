d55c8572b6abcf76de46b081a15db9a730a7359c
DeWayne Cameron <firesbane13@hotmail.com>
2022-01-29 09:24:48 -0600

    Continuing framing things out

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+
+    
     public ShooterSubsystem() {
     }
 
