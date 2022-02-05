57f121076777557af1837da9cf64b641df643d45
PCRobotics <pcrobotics@apps.district279.org>
2022-02-05 13:39:43 -0600

    save point

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
+
     public VisionSubsystem() {
 
     }
